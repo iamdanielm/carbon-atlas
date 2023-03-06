a99b7944b8b9808bab39de23bef45fa4b8cc77df
Daniel <daniel.maydana@cision.com>
2023-03-06 11:28:05 -0400

    Remove carbon-react and install carbon-web-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import TutorialHeader from './components/TutorialHeader';
-
 
 function App() {
   return (
     <div className="App">
-      <TutorialHeader></TutorialHeader>
+      
     </div>
   );
 }
