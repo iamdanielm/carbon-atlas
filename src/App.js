5a8f96f9e5e6fd4f3ff6b760a1a65418158d0815
Daniel <daniel.maydana@cision.com>
2023-03-06 11:16:26 -0400

    Create base project and base example

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TutorialHeader from './components/TutorialHeader';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TutorialHeader></TutorialHeader>
+    </div>
+  );
+}
+
+export default App;
